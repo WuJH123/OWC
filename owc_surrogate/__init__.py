abc0837f85b2dce61816f41e5a93cf560ba34f0b
WuJH123 <125443536+WuJH123@users.noreply.github.com>
2026-08-01 16:51:45 +0100

    Expose OWC prediction API

---
+++ owc_surrogate/__init__.py
@@ -0,0 +1,3 @@
+from .predictor import FEATURE_ORDER, get_model_metadata, predict_efficiency, predict_efficiency_batch, find_optimal_period
+
+__all__ = ["FEATURE_ORDER", "get_model_metadata", "predict_efficiency", "predict_efficiency_batch", "find_optimal_period"]
